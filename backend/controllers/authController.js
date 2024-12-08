2d8dc03bc8d453fceb8de005d6e8d3bdb0f2f6d4
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-12-08 15:15:36 -0500

    implements userModel, adds functions for authController

---
+++ backend/controllers/authController.js
@@ -0,0 +1,105 @@
+import User from "../models/user.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../utils/generateTokenUtils.js";
+
+// determines if email is valid
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+// determines if password matches set requirements
+const validatePassword = (password) => {
+  const minLength = 8;
+  const maxLength = 16;
+  const length = password.length;
+  return length >= minLength && length <= maxLength;
+};
+
+// Create user in database
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!validateEmail(email)) {
+    return res.status(400).json({ message: "Invalid email format!" });
+  }
+
+  if (!validatePassword(password)) {
+    return res
+      .status(400)
+      .json({ message: "Password needs to be between 8 to 16 characters." });
+  }
+
+  try {
+    // Checks if username exists
+    let user = await User.findOne({ username });
+    if (user) {
+      return res.status(400).json({ message: "Username already exists!" });
+    }
+
+    // Checks if email exists
+    user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "Email already exists!" });
+    }
+
+    // Create a new user
+    user = new User({
+      username,
+      email,
+      password,
+    });
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+
+    // Save the user to the database
+    await user.save();
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+// Handle user login
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Check if email exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "Email not found" });
+    }
+
+    // Check if input password matches user password
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(400).json({ message: "Invalid login" });
+    }
+
+    // Generate a token for authenticated user
+    const token = generateToken(user.id);
+
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+// Handle user logout
+export const logout = async (req, res) => {
+  try {
+    res.json({ message: "Successfully Logged Out" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
